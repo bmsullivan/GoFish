d3a22c0a1db0809c259acd19c88d32c9ea78e9d3
Brian Sullivan <bmsullivan@gmail.com>
2013-07-18 22:49:07 -0500

    Card creation and sorting

---
+++ GoFish/App/viewmodels/board.js
@@ -0,0 +1,36 @@
+﻿define(function(require) {
+    var Card = require('viewmodels/card');
+    
+    var board = function () {
+        var self = this;
+
+        self.cards = ko.observableArray(createDeck());
+    };
+
+    function createDeck() {
+        var suits = ['H', 'S', 'C', 'D'];
+        var deck = [];
+        
+        for (var i in suits) {
+            for (var j = 1; j < 14; j++) {
+                deck.push(new Card(suits[i], j));
+            }
+        }
+
+        return shuffle(deck);
+    }
+    
+    function shuffle (array, rng) {
+      var i = array.length, j, swap;
+      if (!rng) rng = Math;
+      while (--i) {
+        j = rng.random() * (i + 1) | 0;
+        swap = array[i];
+        array[i] = array[j];
+        array[j] = swap;
+      }
+      return array;
+    }
+
+    return board;
+});
---
+++ GoFish/App/viewmodels/card.js
@@ -0,0 +1,10 @@
+﻿define(function() {
+    var card = function(suit, rank) {
+        var self = this;
+
+        self.suit = ko.observable(suit || "H");
+        self.rank = ko.observable(rank || 1);
+    };
+
+    return card;
+});
