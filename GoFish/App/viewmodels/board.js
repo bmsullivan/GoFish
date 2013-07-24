57427dc4490bec85a60729b34bd61f893127fec4
bmsullivan <bmsullivan@gmail.com>
2013-07-24 14:53:59 -0500

    Cards drawn to appropriate places

--- GoFish/App/viewmodels/board.js
+++ GoFish/App/viewmodels/board.js
@@ -1,8 +1,9 @@
 ﻿define(function(require) {
     var Card = require('viewmodels/card');
+    var app = require('durandal/app');
     
     var board = function () {
-        var self = this;
+        var self = this, animationCount = 0;
 
         self.cards = ko.observableArray([]);
         self.playerHand = ko.observableArray([]);
@@ -16,6 +17,41 @@
                 self.opponentHand.push(self.cards.pop());
             }
         };
+
+        app.on('drawCard', function (card) {
+            var cardNode = $('.card');
+            var translateX = -(self.playerHand().length * 110) + 151;
+            var keyframes =
+                "@-webkit-keyframes draw" + animationCount + " { " +
+                    "    0% { " +
+                    "             -moz-transform: rotateY(0) rotate(45deg) skew(-18deg, -18deg) scale(.73) translate(-15px, 190px); " +
+                    "             -webkit-transform: rotateY(0) rotate(45deg) skew(-18deg, -18deg) scale(.73) translate(-15px, 190px); " +
+                    "             transform: rotateY(0) rotate(45deg) skew(-18deg, -18deg) scale(.73) translate(-15px, 190px); " +
+                    "         } " +
+                    "    50% { " +
+                    "            -moz-transform: rotateY(180deg) translateX(-100px) translateY(40px) rotate(0); " +
+                    "            -webkit-transform: rotateY(180deg) translateX(-100px) translateY(40px) rotate(0); " +
+                    "            transform: rotateY(180deg) translateX(-100px) translateY(40px) rotate(0); " +
+                    "       } " +
+                    " " +
+                    "    100% { " +
+                    "            -moz-transform: rotateY(180deg) translateX(" + translateX + "px) translateY(200px) rotate(0); " +
+                    "            -webkit-transform: rotateY(180deg) translateX(" + translateX + "px) translateY(200px) rotate(0); " +
+                    "            transform: rotateY(180deg) translateX(" + translateX + "px) translateY(200px) rotate(0); " +
+                    "         } " +
+                    "} ";
+
+            cardNode.on('webkitAnimationEnd', function(event) {
+                self.cards.pop();
+                self.playerHand.push(card);
+            });
+            
+            document.styleSheets[document.styleSheets.length - 1].insertRule(keyframes, 0);
+            cardNode[0].style.webkitAnimation = 'draw' + animationCount + ' 2s forwards';
+
+            animationCount++;
+            
+        });
 
         self.startNewGame();
     };
--- GoFish/App/viewmodels/card.js
+++ GoFish/App/viewmodels/card.js
@@ -1,13 +1,15 @@
-﻿define(function() {
+﻿define(function (require) {
+
+    var app = require('durandal/app');
+
     var card = function(suit, rank) {
         var self = this;
 
         self.suit = ko.observable(suit || "♥");
         self.rank = ko.observable(rank || 'A');
-        self.isFlipped = ko.observable(false);
 
-        self.turnCardOver = function() {
-            self.isFlipped(!self.isFlipped());
+        self.draw = function () {
+            app.trigger('drawCard', this);
         };
     };
 
